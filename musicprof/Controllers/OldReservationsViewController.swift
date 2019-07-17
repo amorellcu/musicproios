b33dd6997f72b0e9a3dde33e80788fe31994ae40
Alexei Labrada <a.labrada@yandex.ru>
2019-07-17 19:21:18 +0300

    Filter the list of received classes.

---
+++ musicprof/Controllers/OldReservationsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  OldReservationsViewController.swift
+//  musicprof
+//
+//  Created by John Doe on 7/17/19.
+//  Copyright © 2019 Alexis Morell Blanco. All rights reserved.
+//
+
+import UIKit
+
+class OldReservationsViewController: ReservationListViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func loadReservations(_ reservations: [Reservation]) {
+        let date = Date()
+        self.classes = reservations.lazy.compactMap({$0.classes}).filter({$0.date < date})
+    }
+}
